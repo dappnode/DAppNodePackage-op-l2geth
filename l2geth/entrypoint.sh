53347d198b27cfee17a9be614ea21181f50cca82
Diego <dsimog01@estudiantes.unileon.es>
2023-09-27 12:45:09 +0200

    Remove envs

--- l2geth/entrypoint.sh
+++ l2geth/entrypoint.sh
@@ -2,4 +2,10 @@
 
 # Configuration defined in https://community.optimism.io/docs/developers/bedrock/node-operator-guide/#legacy-geth
 geth --datadir /data \
+    --http \
+    --http.addr 0.0.0.0 \
+    --http.corsdomain "*" \
+    --http.api eth,rollup,net,web3,debug \
+    --http.vhosts "*" \
+    --http.port 8545 \
     ${EXTRA_FLAGS}
